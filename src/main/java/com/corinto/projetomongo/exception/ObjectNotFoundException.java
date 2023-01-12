d75655e09b834a932cbe16c559bdbdec68664799
FHOCFHOC <backintheusa@gmail.com>
2023-01-12 18:57:29 -0300

    last commit

---
+++ src/main/java/com/corinto/projetomongo/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.corinto.projetomongo.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
